7b893b63e3411700457e150ae72d079b935461bc
Pornmaifa <pornmaifa.ru.66@ubu.ac.th>
2026-03-19 20:31:46 +0700

    19/3/69

--- car/settings.py
+++ car/settings.py
@@ -88,16 +88,6 @@ WSGI_APPLICATION = 'car.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'car',        
-        'USER': 'root',        
-        'PASSWORD': 'Wsmai47',  
-        'HOST': 'localhost',             
-        'PORT': '3306',
-    }
-}
 
 
 # Password validation
